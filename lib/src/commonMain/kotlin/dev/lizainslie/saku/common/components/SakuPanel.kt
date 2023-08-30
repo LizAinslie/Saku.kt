d43dcc7c24752193e4096f7b16787b6765072548
Liz Ainslie <lizainslie16@gmail.com>
2023-08-30 06:12:03 -0700

    chore(lib): cleanup panel component

--- lib/src/commonMain/kotlin/dev/lizainslie/saku/common/components/SakuPanel.kt
+++ lib/src/commonMain/kotlin/dev/lizainslie/saku/common/components/SakuPanel.kt
@@ -1,8 +1,6 @@
 package dev.lizainslie.saku.common.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,10 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.lizainslie.saku.common.theme.SakuTheme
@@ -25,7 +21,6 @@ import dev.lizainslie.saku.common.util.generateCornerBoxPath
 fun SakuPanel(
     modifier: Modifier = Modifier,
     corner: Dp = 5.dp,
-    border: Color = colorWhitePurple,
     background: Color = colorBlackPurple,
     foreground: Color = colorWhitePurple,
     padding: PaddingValues = PaddingValues(), // default zero
