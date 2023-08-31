fb44e47933c82eb861334f65952a1ff111d72e5a
Liz Ainslie <lizainslie16@gmail.com>
2023-08-31 12:51:53 -0700

    feat(lib): image component

---
+++ lib/src/commonMain/kotlin/dev/lizainslie/saku/common/components/SakuImage.kt
@@ -0,0 +1,30 @@
+package dev.lizainslie.saku.common.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import dev.lizainslie.saku.common.util.CornerBoxShape
+import dev.lizainslie.saku.common.util.Corners
+
+@Composable
+fun SakuImage(
+    painter: Painter,
+    contentDescription: String? = null,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.None,
+    corner: Dp = 5.dp,
+    corners: Corners = Corners(),
+) {
+    Image(
+        painter = painter,
+        contentDescription = contentDescription,
+        contentScale = contentScale,
+        modifier = modifier.clip(CornerBoxShape(PaddingValues(), corner, corners))
+    )
+}
