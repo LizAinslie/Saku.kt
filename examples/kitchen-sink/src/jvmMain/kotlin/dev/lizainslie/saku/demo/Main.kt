a67300b828296fc3125dccb49c455adeeb8e732c
Liz Ainslie <lizainslie16@gmail.com>
2023-09-04 19:44:51 -0700

    feat(demo): update kitchen sink example to match new scaffold

--- examples/kitchen-sink/src/jvmMain/kotlin/dev/lizainslie/saku/demo/Main.kt
+++ examples/kitchen-sink/src/jvmMain/kotlin/dev/lizainslie/saku/demo/Main.kt
@@ -28,107 +28,96 @@ import dev.lizainslie.saku.common.util.Extrude
 @Preview
 fun App() {
     SakuTheme {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            SakuText("Saku.kt Demo", style = SakuTheme.typography.headingH1)
-//            SakuPanel(
-//                padding = PaddingValues(10.dp),
-//                corner = 12.5.dp
-//            ) {
-//                Row(
-//                    horizontalArrangement = Arrangement.spacedBy(5.dp),
-//                    verticalAlignment = Alignment.CenterVertically,
-//                ) {
-//                    SakuText("Some Testing Text", modifier = Modifier.padding(5.dp, 0.dp))
-//                    CounterButton(Corners(topRight = false))
-//                    CounterButton(Corners(bottomLeft = false))
-//                }
-//            }
+        Scaffold {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                SakuText("Saku.kt Demo", style = SakuTheme.typography.headingH1)
 
-            Row {
-                SakuPanel(
-                    corner = 12.5.dp,
-                    modifier = Modifier.width(300.dp)
-                ) {
-                    Column (modifier = Modifier.width(IntrinsicSize.Max)) {
-                        SakuImage(
-                            painter = painterResource("img/lizzy.png"),
-                            contentScale = ContentScale.FillWidth,
-                            corners = Corners.TopRight,
-                            extrude = Extrude.BottomRight,
-                            corner = 12.5.dp,
-                        )
-
-                        Column(
-                            modifier = Modifier.padding(10.dp),
-                            verticalArrangement = Arrangement.spacedBy(5.dp),
-                        ) {
-                            SakuText(
-                                "Liz Ainslie",
-                                style = SakuTheme.typography.headingH2.copy(
-                                    fontSize = 24.sp,
-                                    color = SakuTheme.colors.foreground
-                                ),
-                                modifier = Modifier.padding(),
-                                lineHeight = 16.sp,
-                                overflow = TextOverflow.Ellipsis
+                Row {
+                    SakuPanel(
+                        corner = 12.5.dp,
+                        modifier = Modifier.width(300.dp)
+                    ) {
+                        Column (modifier = Modifier.width(IntrinsicSize.Max)) {
+                            SakuImage(
+                                painter = painterResource("img/lizzy.png"),
+                                contentScale = ContentScale.FillWidth,
+                                corners = Corners.TopRight,
+                                extrude = Extrude.BottomRight,
+                                corner = 12.5.dp,
                             )
 
-                            SakuText(
-                                """
+                            Column(
+                                modifier = Modifier.padding(10.dp),
+                                verticalArrangement = Arrangement.spacedBy(5.dp),
+                            ) {
+                                SakuText(
+                                    "Liz Ainslie",
+                                    style = SakuTheme.typography.headingH2.copy(
+                                        fontSize = 24.sp,
+                                        color = SakuTheme.colors.foreground
+                                    ),
+                                    modifier = Modifier.padding(),
+                                    lineHeight = 16.sp,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+
+                                SakuText(
+                                    """
                                     Meow mrrp nya~ meow meow meow mrrrrrrrrp
                                 """.trimIndent(),
-                                overflow = TextOverflow.Ellipsis,
-                                maxLines = 4,
-                            )
+                                    overflow = TextOverflow.Ellipsis,
+                                    maxLines = 4,
+                                )
 
-                            Row(
-                                horizontalArrangement = Arrangement.spacedBy(5.dp),
-                                modifier = Modifier.fillMaxWidth(),
-                            ) {
-                                SakuButton(
-                                    modifier = Modifier.fillMaxWidth(1f / 3),
-                                    corners = Corners.BottomLeft
-                                ) {
-                                    SakuIcon(
-                                        imageVector = FeatherIcons.UserPlus,
-                                        "follow icon",
-                                        color = colorDark,
-                                        modifier = Modifier.size(16.dp),
-                                    )
-                                    SakuText("Follow")
-                                }
-
-                                SakuButton(
-                                    modifier = Modifier.fillMaxWidth(1f / 2),
-                                    corners = Corners.None,
-                                    theme = SakuButtonTheme.Secondary,
-                                ) {
-                                    SakuIcon(
-                                        imageVector = FeatherIcons.MessageCircle,
-                                        "chat icon",
-                                        color = colorLight,
-                                        modifier = Modifier.size(16.dp),
-                                    )
-                                    SakuText("Chat")
-                                }
-
-
-                                SakuButton(
+                                Row(
+                                    horizontalArrangement = Arrangement.spacedBy(5.dp),
                                     modifier = Modifier.fillMaxWidth(),
-                                    corners = Corners.TopRight,
-                                    theme = SakuButtonTheme.Danger,
                                 ) {
-                                    SakuIcon(
-                                        imageVector = FeatherIcons.UserX,
-                                        "block icon",
-                                        color = colorLight,
-                                        modifier = Modifier.size(16.dp),
-                                    )
-                                    SakuText("Block")
+                                    SakuButton(
+                                        modifier = Modifier.fillMaxWidth(1f / 3),
+                                        corners = Corners.BottomLeft
+                                    ) {
+                                        SakuIcon(
+                                            imageVector = FeatherIcons.UserPlus,
+                                            "follow icon",
+                                            color = colorDark,
+                                            modifier = Modifier.size(16.dp),
+                                        )
+                                        SakuText("Follow")
+                                    }
+
+                                    SakuButton(
+                                        modifier = Modifier.fillMaxWidth(1f / 2),
+                                        corners = Corners.None,
+                                        theme = SakuButtonTheme.Secondary,
+                                    ) {
+                                        SakuIcon(
+                                            imageVector = FeatherIcons.MessageCircle,
+                                            "chat icon",
+                                            color = colorLight,
+                                            modifier = Modifier.size(16.dp),
+                                        )
+                                        SakuText("Chat")
+                                    }
+
+
+                                    SakuButton(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        corners = Corners.TopRight,
+                                        theme = SakuButtonTheme.Danger,
+                                    ) {
+                                        SakuIcon(
+                                            imageVector = FeatherIcons.UserX,
+                                            "block icon",
+                                            color = colorLight,
+                                            modifier = Modifier.size(16.dp),
+                                        )
+                                        SakuText("Block")
+                                    }
                                 }
                             }
                         }
